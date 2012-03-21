6d9f482ad485f1156252bbf16903aef09c4b53d0
Steven Wu <stevenhwu@gmail.com>
2012-03-21 13:22:24 -0400

    Revert "re-merge old run_ext_prog"
    This reverts commit 081680982a73f4bcd5580c34319723c4cdbfca59.

--- src/core/run_ext_prog.py
+++ src/core/run_ext_prog.py
@@ -4,96 +4,65 @@ Created on Jan 23, 2012
 @author: Steven Wu
 '''
 import subprocess
-import os
-import sys
-
-def get_platform():
-    if sys.platform.startswith('linux'):
-        platform="linux"
-    elif sys.platform.startswith('darwin'):
-        platform="mac"
-    else:
-        print "Unsupport OS: %s" % sys.platform
-        sys.exit(-1)
-    return platform
-
+import copy
 
 class runExtProg(object):
     '''
-    self.program_name: program_name
-    self._switch: [list], contain all switches required to run the program_name
-        self.parameter = property(_switch) 
+    self.program: program name
+    self._switch: [list], contain all switches required to run the program
     self.cwd: working directory
     self.output: capture output message
     self.errors: capture error message
     '''
-    
-    platform = get_platform()
-    
-    def __init__(self, p, pdir=None, len=0, checkOS=False):
+
+
+    def __init__(self, p, pdir=None, len=0):
         
         self.program_name = p
+
         self.init_switch(len)
         self.cwd = pdir
-        self.checkPlatform(checkOS)
-#            print "zz: ", is_exe(self.cwd+self.program_name)
+
 
     def set_param_at(self, param, position):
-        self._switch[position - 1] = str(param)
+        self._switch[position-1]=str(param)
 
 
-    def run(self):
-        self._command = [self.program_name]
-        self._command.extend(self._switch)
-        p = subprocess.Popen(self._command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.cwd)
-        self.output, self.errors = p.communicate()
-        
-    
+    def init_switch(self, leng):
+        self._switch = [None]*leng
 
     def get_switch(self):
         return self._switch
-    
-    def set_switch(self, s):    
+
+
+    """
+    ignore here here
+    """
+    def set_switch(self, s):
         self.reset_switch()
         self.add_switch(s);
-        
-    parameter = property(get_switch, set_switch, doc="switch/parameters") 
+    parameters = property( get_switch, set_switch, doc="switch/parameters" ) 
     
-    def init_switch(self, leng):
-        self._switch = [None] * leng
-
-
-    '''
-    ignore below for now
-    '''
-    def checkPlatform(self, checkOS):
-        """
-        Check platform, only check program_name start with "./"
-        """
-        if checkOS or self.program_name.find("./") is 0:
-            self.name_only = self.program_name[2:len(self.program_name)]
-            self.name_only = self.name_only + "_" + runExtProg.platform
-            if os.path.exists(self.cwd+self.name_only):
-                self.program_name = "./"+self.name_only
-
     
-    def add_switch(self, s):
-        if isinstance(s, str):
-            self._switch.append(s)
-        elif isinstance(s, list):
-            self._switch.extend(s)
-#        print "add_switch: ",s,"\t==",self._switch
+    def run(self):
+        self._command = [self.program_name]
+        self._command.extend(self._switch)
+        p = subprocess.Popen(self._command,stdout= subprocess.PIPE, stderr=subprocess.PIPE, cwd = self.cwd )
+        self.output, self.errors = p.communicate()
+
+
+
+
+
 
     def reset_switch(self):
         self._switch = list()
-
-
     
     def updateSwitch(self, switchName, switchValue):
         switchValue = str(switchValue)
         if switchName in self._switch:
             self._index = self._switch.index(switchName)
-            self._switch[self._index + 1] = switchValue
+            self._switch[self._index+1] = switchValue
         else:
             self.add_switch([switchName, switchValue])
             
@@ -116,25 +85,9 @@ class runExtProg(object):
 
 
 
-'''
-Global
-'''
-
-
-def which(program):
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
-def is_exe(fpath):
-    return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-        
+    def add_switch(self, s):
+        if isinstance(s, str):
+            self._switch.append(s)
+        elif isinstance(s, list):
+            self._switch.extend(s)
+#        print "add_switch: ",s,"\t==",self._switch
